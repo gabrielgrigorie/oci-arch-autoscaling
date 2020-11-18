0c23d49b7ddab4ecedd18b9de2570d313aec82e4
mlinxfeld <mlinxfeld@foggykitchen.com>
2020-11-18 16:09:23 +0100

    First draft

---
+++ compute.tf
@@ -0,0 +1,135 @@
+## Copyright © 2020, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+# This Terraform script provisions a compute instance, instance configuration, instance pool and autoscaling config.
+
+# Create Compute Instance
+
+resource "oci_core_instance" "compute_instance1" {
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[var.availability_domain - 2]["name"]
+  compartment_id      = var.compartment_ocid
+  display_name        = "Web-Server-1"
+  shape               = var.instance_shape
+  fault_domain        = "FAULT-DOMAIN-1"
+
+  source_details {
+    source_type             = "image"
+    source_id               = data.oci_core_images.InstanceImageOCID.images[0].id
+    boot_volume_size_in_gbs = "50"
+  }
+
+  create_vnic_details {
+     subnet_id = oci_core_subnet.subnet_2.id
+     nsg_ids = [oci_core_network_security_group.WebSecurityGroup.id, oci_core_network_security_group.SSHSecurityGroup.id]
+  }
+
+  metadata = {
+    ssh_authorized_keys = tls_private_key.public_private_key_pair.public_key_openssh
+  }
+
+  timeouts {
+    create = "60m"
+  }
+}
+
+# Create Instacne Image
+
+resource "oci_core_image" "flask_instance_image" {
+    depends_on = [null_resource.compute-script1]
+    compartment_id = var.compartment_ocid
+    instance_id = oci_core_instance.compute_instance1.id
+}
+
+# Create Instacne Configuration
+
+resource "oci_core_instance_configuration" "instance_configuration" {
+    
+    depends_on = [oci_core_image.flask_instance_image, null_resource.compute-script1]
+    compartment_id = var.compartment_ocid
+    display_name   = "Instance_Configuration"
+    instance_details { 
+        instance_type = "compute"
+        launch_details {
+            compartment_id = var.compartment_ocid
+            shape          = var.instance_shape
+            source_details {
+                source_type = "image"
+                image_id = oci_core_image.flask_instance_image.id
+            }
+            create_vnic_details {
+                subnet_id = oci_core_subnet.subnet_2.id
+                nsg_ids = [oci_core_network_security_group.WebSecurityGroup.id, oci_core_network_security_group.SSHSecurityGroup.id]
+            }        
+        }
+    }
+}
+
+# Create Instance Pool
+
+resource "oci_core_instance_pool" "instance_pool" {
+    compartment_id = var.compartment_ocid
+    instance_configuration_id = oci_core_instance_configuration.instance_configuration.id  
+    placement_configurations {
+        availability_domain = data.oci_identity_availability_domains.ads.availability_domains[var.availability_domain - 2]["name"]
+        primary_subnet_id = oci_core_subnet.subnet_2.id
+
+    }
+    size = "2"
+    display_name = "Instance_Pool"
+    load_balancers {
+        backend_set_name = oci_load_balancer_backend_set.lb-bes1.name
+        load_balancer_id = oci_load_balancer.lb1.id
+        port = "5000"
+        vnic_selection = "PrimaryVnic"
+    }
+}
+
+# Create Autoscaling Configuration
+
+resource "oci_autoscaling_auto_scaling_configuration" "autoscaling_configuration" {
+    auto_scaling_resources {
+
+        id = oci_core_instance_pool.instance_pool.id
+        type = "instancePool"
+    }
+    compartment_id = var.compartment_ocid
+    policies {
+        display_name = "Threshold_AutoScaling_Configuration_Policies"
+        capacity {
+            initial = "2"
+            max = "4"
+            min = "2"
+        }
+        policy_type = "threshold"
+        rules {
+            action {
+                type = "CHANGE_COUNT_BY"
+                value = "1"
+            }
+            display_name = "Threshold_AutoScaling_Configuration_Policies_ScaleOut_Rule"
+            metric {
+                metric_type = "CPU_UTILIZATION"
+                threshold {
+                    operator = "GT"
+                    value = "80"
+                }
+            }
+        }
+        rules {
+            action {
+                type  = "CHANGE_COUNT_BY"
+                value = "-1"
+            }
+            display_name = "Threshold_AutoScaling_Configuration_Policies_ScaleInRule"
+            metric {
+                metric_type = "CPU_UTILIZATION"
+                threshold {
+                    operator = "LT"
+                    value = "20"
+                }
+            }
+        }
+    }
+    cool_down_in_seconds = "300"
+    display_name = "Threshold_AutoScaling_Configuration"
+}
---
+++ variables.tf
@@ -0,0 +1,86 @@
+## Copyright © 2020, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+# Variables
+variable "tenancy_ocid" {}
+variable "compartment_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+#variable "ssh_public_key" {}
+#variable "ssh_private_key" {}
+variable "ATP_password" {}
+
+# Specify any Default Value's here
+
+variable "availability_domain" {
+  default = "3"
+}
+
+# OS Images
+variable "instance_os" {
+  description = "Operating system for compute instances"
+  default     = "Oracle Linux"
+}
+
+variable "linux_os_version" {
+  description = "Operating system version for all Linux instances"
+  default     = "7.8"
+}
+
+variable "instance_shape" {
+  description = "Instance Shape"
+  default     = "VM.Standard2.1"
+}
+
+/*
+we are specifying username's and password's to be used here for the app. 
+However specify the username and password of your choice if you want to change it.
+*/
+
+variable "ATP_database_cpu_core_count" {
+  default = 1
+}
+
+variable "ATP_database_data_storage_size_in_tbs" {
+  default = 1
+}
+
+variable "ATP_database_db_name" {
+  default = "aTFdb"
+}
+
+variable "ATP_database_db_version" {
+  default = "19c"
+}
+
+variable "ATP_database_defined_tags_value" {
+  default = "value"
+}
+
+variable "ATP_database_display_name" {
+  default = "ATP"
+}
+
+variable "ATP_database_freeform_tags" {
+  default = {
+    "Owner" = "ATP"
+  }
+}
+
+variable "ATP_database_license_model" {
+  default = "LICENSE_INCLUDED"
+}
+
+variable "ATP_tde_wallet_zip_file" {
+  default = "tde_wallet_aTFdb.zip"
+}
+
+variable "ATP_private_endpoint_label" {
+  default = "ATPPrivateEndpoint"
+}
+
+variable  "ATP_data_guard_enabled" {
+  default = false 
+}
