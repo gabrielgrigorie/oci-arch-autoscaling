cd4e07f7c692123a5243e1662207abde74e6f937
lfeldman <lukasz.feldman@oracle.com>
2020-11-23 10:46:58 +0100

    Fixes on priv subnet

--- route_tables.tf
+++ route_tables.tf
@@ -3,7 +3,7 @@
 
 # Create route table to connect vcn to internet gateway
 
-resource "oci_core_route_table" "rt" {
+resource "oci_core_route_table" "rt-pub" {
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_virtual_network.vcn.id
   display_name   = "rt-table"
@@ -11,5 +11,16 @@ resource "oci_core_route_table" "rt" {
     cidr_block        = "0.0.0.0/0"
     network_entity_id = oci_core_internet_gateway.ig.id
   }
+}
+
+resource "oci_core_route_table" "rt-priv" {
+    compartment_id = var.compartment_ocid
+    vcn_id         = oci_core_virtual_network.vcn.id
+    display_name = "rt-table-priv"
+    route_rules {
+        destination       = "0.0.0.0/0"
+        destination_type  = "CIDR_BLOCK"
+        network_entity_id = oci_core_nat_gateway.nat_gw.id
+    }
 }
 
